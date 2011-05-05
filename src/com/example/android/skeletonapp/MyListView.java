c3ba42d0a49d90f57b29dcce0306d9858045d068
Arvid Böttiger <bottiger@gmail.com>
2011-05-05 07:18:02 +0200

    Improved ListView

--- src/com/example/android/skeletonapp/MyListView.java
+++ src/com/example/android/skeletonapp/MyListView.java
@@ -2,16 +2,16 @@ package com.example.android.skeletonapp;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashMap;
 
 import sncatalog.shared.MobileEpisode;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
-import android.app.ListActivity; 
  
 public class MyListView extends ListActivity 
 {
@@ -31,7 +31,8 @@ public class MyListView extends ListActivity
             ef.getEpisode(200).getTitle()
     };
     */
-	String[] episodes = {};
+	ArrayList<MobileEpisode> episodes = null;
+	static final ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
  
     @Override  
     public void onCreate(Bundle savedInstanceState) 
@@ -51,31 +52,43 @@ public class MyListView extends ListActivity
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		SimpleAdapter adapter = new SimpleAdapter(
+		this,
+		list,
+		R.layout.list_item,
+		new String[] {"number", "title"},
+		new int[] {R.id.number, R.id.title}
+		);
+		this.makeAdapter(episodes);
+		setListAdapter(adapter);
  
-        setListAdapter(new ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_1, episodes));
+        //setListAdapter(new ArrayAdapter<String>(this,
+        //    android.R.layout.simple_list_item_1, episodes));
+        //setListAdapter(new ArrayAdapter<String>(this,
+        //       R.layout.list_item, episodes));
     }    
  
-    private String[] getEpisodes() throws IOException, ClassNotFoundException, URISyntaxException {
+    private ArrayList<MobileEpisode> getEpisodes() throws IOException, ClassNotFoundException, URISyntaxException {
     	EpisodeFetcher ef = new EpisodeFetcher();
-    	
-    	ArrayList<MobileEpisode> lme = ef.getEpisodes();
-		String[] episodes = new String[lme.size()];
-		
-		int i = 0;
-		for(MobileEpisode me : lme) {
-			episodes[i] = me.getTitle();
-			i++;
-		}
-		return episodes;
+    	return ef.getEpisodes();
 	}
+    
+    private void makeAdapter(ArrayList<MobileEpisode> me) {
+    	for (MobileEpisode e : me) {
+    		HashMap<String,String> map = new HashMap<String,String>();
+    		map.put("number", e.getLink());
+    		map.put("title", e.getTitle());
+    		list.add(map);
+    	}
+    }
 
 	public void onListItemClick(
     ListView parent, View v,
     int position, long id) 
     {   
         Toast.makeText(this, 
-            "You have selected " + episodes[position], 
+            "You have selected ",
             Toast.LENGTH_SHORT).show();
     }  
 	
